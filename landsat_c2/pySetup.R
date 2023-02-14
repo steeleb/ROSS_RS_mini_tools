22a54ac377e34db89e7eea47a6421575c9b9b7ad
B Steele <B.Steele@colostate.edu>
2023-02-14 14:54:26 -0700

    tweaks, still working on dswe counts

--- landsat_c2/pySetup.R
+++ landsat_c2/pySetup.R
@@ -5,16 +5,13 @@ library('tidyverse')
 try(install_miniconda())
 py_install(c('earthengine-api', 'pandas', 'geopandas'))
 
-#grab your current WD
-dir = getwd()
-
 #create a conda environment named 'apienv' with the packages you need
-conda_create(envname = file.path(dir, 'env'),
+conda_create(envname = file.path(py_env_dir, 'env'),
              packages = c('earthengine-api', 'pandas', 'geopandas'))
-Sys.setenv(RETICULATE_PYTHON = file.path(dir, 'env/bin/python/'))
-use_condaenv("env/")
+Sys.setenv(RETICULATE_PYTHON = file.path(py_env_dir, 'env/bin/python/'))
+use_condaenv(file.path(py_env_dir,"env/"))
 #print the configuration
 py_config()
 
 #check install
-py_install(packages = c('earthengine-api', 'pandas', 'geopandas'), envname = 'env/')
+py_install(packages = c('earthengine-api', 'pandas', 'geopandas'), envname = file.path(py_env_dir, 'env/'))
